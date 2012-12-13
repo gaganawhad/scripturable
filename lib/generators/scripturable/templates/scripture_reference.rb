d18060ad1de126d8f878fc100b97b986faac029d
Gagan Awhad <gagan.awhad@desiringgod.org>
2012-12-12 22:41:09 -0600

    DRYing up by using the REFERENCE_FORMAT constant

--- lib/generators/scripturable/templates/scripture_reference.rb
+++ lib/generators/scripturable/templates/scripture_reference.rb
@@ -1,10 +1,12 @@
 class ScriptureReference < ActiveRecord::Base
+  REFERENCE_FORMAT = /^(\d+)(?::(\d+))?(?::(\d+))?$/ #http://rubular.com/r/7axgNYfYiK
+
   belongs_to :scripturable, :polymorphic => true
 
   validates_presence_of :start_at
-  validates :start_at, :format => {:with =>/^\d+(?:(?::\d+)?){1,2}$/, #http://rubular.com/r/7axgNYfYiK
+  validates :start_at, :format => {:with => REFRENCE_FORMAT, 
     :message => "Starting reference should be of the format '<book-number>(:<chapter-number>(:<verse-number>))" }
-  validates :end_at, :format => {:with =>/^\d+(?:(?::\d+)?){1,2}$/, #http://rubular.com/r/7axgNYfYiK
+  validates :end_at, :format => {:with => REFERENCE_FORMAT, 
     :message => "Ending reference should be of the format '<book-number>(:<chapter-number>(:<verse-number>))" }
 
 
