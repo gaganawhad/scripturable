2cc2a7d51dde17c24b94b5243768f95c5f5b67e2
Gagan Awhad <gagan.awhad@desiringgod.org>
2012-12-12 18:57:10 -0600

    Adding a few validators especially those that check the format of start and end verse

--- lib/generators/scripturable/templates/scripture_reference.rb
+++ lib/generators/scripturable/templates/scripture_reference.rb
@@ -1,4 +1,9 @@
 class ScriptureReference < ActiveRecord::Base
   belongs_to :scripturable, :polymorphic => true
+  validates_presence_of :start_verse
+  validates :start_verse, :format => {:with =>/^\d+(?:(?::\d+)?){1,2}$/, #http://rubular.com/r/7axgNYfYiK
+    :message => "Scripture Verse should be of the format '<book-number>(:<chapter-number>(:<verse-number>))" }
+  validates :end_verse, :format => {:with =>/^\d+(?:(?::\d+)?){1,2}$/, #http://rubular.com/r/7axgNYfYiK
+    :message => "Scripture Verse should be of the format '<book-number>(:<chapter-number>(:<verse-number>))" }
 
 end
