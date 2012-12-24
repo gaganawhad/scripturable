20db3f60deb020d032df03715bbd1d2114fc5fc3
Gagan Awhad <gagan.awhad@desiringgod.org>
2012-12-23 22:53:41 -0600

    Latest version of code trying to work out validation of existing scripture references

--- lib/generators/scripturable/templates/scripture_reference.rb
+++ lib/generators/scripturable/templates/scripture_reference.rb
@@ -9,6 +9,7 @@ class ScriptureReference < ActiveRecord::Base
   validates :end_at, :format => {:with => REFERENCE_FORMAT, 
     :message => "end reference should follow the format '<book-number>(:<chapter-number>(:<verse-number>))" }
 
+  
   def start_book
     start_reference['book']
   end
@@ -52,6 +53,57 @@ class ScriptureReference < ActiveRecord::Base
 
   def follows_format? reference
     reference.scan(REFERENCE_FORMAT).any?
+  end
+
+  def reference_exists? reference
+    raise "Scripture reference does not follow the format: '<book-number>(:<chapter-number>(:<verse-number>))'" unless follows_format? reference
+
+    book_number, chapter_number, verse_number = parse_reference(reference).values
+    answer = catch(:message) do
+      if book_exists? book_number
+        scripture_book = find_scripture_book book_number
+        if chapter_number.present?
+          if chapter_exists? scripture_book, verse_number
+            scripture_chapter = find_scripture_chapter(chapter_number)
+            if verse_number.present?
+              if 1..scripture_chapter['verse_count']).include? verse_number
+                "Scripture reference exists"
+              else
+                "Book does not exist"
+              end
+            else
+              "Scripture reference exists"
+            end
+          else
+            "Book does not exist"
+          end
+        else 
+          "Scripture reference exists"
+        end
+      else #book does not exist
+        "Book does not exist"
+      end
+    end
+  end
+
+  def book_exists? book_number
+    (1..66).include? book_number #66 books in the Bible
+  end
+
+  def chapter_exists? scripture_book, chapter_number
+    find_scripture_chapter(scripture_book, chapter_number).present?
+  end
+
+  def verse_exists? scripture_chapter, verse_number
+    (1..scripture_chapter['verse_count']).include? verse_number 
+  end
+
+  def find_scripture_book book_number
+    Scripturable::SCRIPTURE_META.find{|book| book['book_number'] == reference_hash['book']}
+  end
+
+  def find_scripture_chapter scripture_book, chapter_number
+    scripture_book['chapters'].find{|chapter| chapter['chapter_number'] == chapter_number}
   end
 
 end
