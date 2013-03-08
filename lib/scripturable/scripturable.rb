9bff2e3fc7923349a3f85e41f4cfae0361e493c6
Gagan Awhad <gagan.awhad@desiringgod.org>
2013-03-08 12:46:29 -0600

    I imagine needing a has_many association between resources and scripture_references by default

--- lib/scripturable/scripturable.rb
+++ lib/scripturable/scripturable.rb
@@ -4,8 +4,12 @@ module Scripturable
   end
 
   module ClassMethods
-    def scripturable
-      has_one :scripture_reference, :as => :scripturable, :dependent => :destroy
+    def scripturable options={}
+      if options[:has_one] == true
+        has_one :scripture_reference, :as => :scripturable, :dependent => :destroy 
+      else
+        has_many :scripture_references, :as => :scripturable, :dependent => :destroy
+      end
     end
   end
 
