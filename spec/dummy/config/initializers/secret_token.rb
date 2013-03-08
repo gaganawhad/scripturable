89675b123d2747e5ed5fb2656b7f1c0066173e72
Gagan Awhad <gagan.awhad@desiringgod.org>
2013-03-08 11:46:52 -0600

    Remving dummy app from the respository for this gem

--- spec/dummy/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = '5cc862548c69c3c9842319a10d3ba6d9b6150c609063f01b22410bbeac47e7dfb231e3fd8a10c8cf5dcf8d1fddb27eb9e40d39d23bf19eb3a224cb23d352a5e7'
--- spec/dummy/db/migrate/20121202013110_create_resources.rb
+++
@@ -1,9 +0,0 @@
-class CreateResources < ActiveRecord::Migration
-  def change
-    create_table :resources do |t|
-      t.string :title
-
-      t.timestamps
-    end
-  end
-end
