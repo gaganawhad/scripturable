0ac369a2577201c3cfda2ec01e28f72f5279e7fb
Gagan Awhad <gagan.awhad@desiringgod.org>
2013-03-11 22:51:14 -0500

    accepting nested attributes for scripture_references and making them accessible for mass-assignment

--- lib/scripturable/scripturable.rb
+++ lib/scripturable/scripturable.rb
@@ -6,9 +6,13 @@ module Scripturable
   module ClassMethods
     def scripturable options={}
       if options[:has_one] == true
+        attr_accessible :scripture_references_attributes
         has_one :scripture_reference, :as => :scripturable, :dependent => :destroy 
+        accepts_nested_attributes_for :scripture_reference
       else
+        attr_accessible :scripture_references_attributes
         has_many :scripture_references, :as => :scripturable, :dependent => :destroy
+        accepts_nested_attributes_for :scripture_references
       end
     end
   end
