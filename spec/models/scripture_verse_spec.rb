e684a2f46a99d22deb3b740c690a431f32929fe7
Gagan Awhad <gagan.awhad@desiringgod.org>
2013-03-15 12:21:20 -0500

    No longer want to raise errors on initialization

--- spec/models/scripture_verse_spec.rb
+++ spec/models/scripture_verse_spec.rb
@@ -3,11 +3,6 @@ require 'spec_helper'
 describe ScriptureVerse do
   describe '#initialize' do
 
-    it 'should raise an error if it does not follow the format' do
-      lambda {ScriptureVerse.new("8:12-14")}.should raise_error(RuntimeError,"Scripture reference does not follow the format: '<book-number>(:<chapter-number>(:<verse-number>))'")
-    end
-
-
     it 'should initializse the scripture reference when the format is valid' do
       lambda {@scripture_verse = ScriptureVerse.new("8:12:14")}.should_not raise_error(RuntimeError,"Scripture reference does not follow the format: '<book-number>(:<chapter-number>(:<verse-number>))'")
       @scripture_verse.should be_present
