e259c7c9e8bfa4d24e05df5cb37a3ce8bbc2107d
Gagan Awhad <gagan.awhad@desiringgod.org>
2013-03-19 17:16:39 -0500

    Added scope to get all devotionals from a scripture book

--- lib/scripturable/scripturable.rb
+++ lib/scripturable/scripturable.rb
@@ -14,6 +14,20 @@ module Scripturable
         has_many :scripture_references, :as => :scripturable, :dependent => :destroy
         accepts_nested_attributes_for :scripture_references, :allow_destroy => true
       end
+
+      def self.from_scripture_book book
+        book_number = case book
+        when String
+          book_hash = Scripturable::SCRIPTURE_BOOKS_META.find{|meta_book| meta_book['string_id'] == book.parameterize} 
+          raise "book #{book} was not found" unless book_hash
+          book_hash['number']
+        when Fixnum
+          raise "book #{book} was not found" unless book.between?(1,66)
+          book
+        end
+        self.includes(:scripture_references).where("scripture_references.start_at >= #{book_number * 1000000} AND scripture_references.start_at < #{(book_number + 1) * 1000000}").uniq
+      end
+
     end
   end
 
