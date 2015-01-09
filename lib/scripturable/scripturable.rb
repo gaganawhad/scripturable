c43eecd9cace9fe88cea110ba40b4fb01ce50970
Gagan Awhad <gagan.a.awhad@gmail.com>
2015-01-09 15:26:20 +0530

    attr_acessible is depreicated for strong parameters

--- lib/scripturable/scripturable.rb
+++ lib/scripturable/scripturable.rb
@@ -6,11 +6,9 @@ module Scripturable
   module ClassMethods
     def scripturable options={}
       if options[:has_one] == true
-        attr_accessible :scripture_references_attributes
         has_one :scripture_reference, :as => :scripturable, :dependent => :destroy 
         accepts_nested_attributes_for :scripture_reference, :allow_destroy => true
       else
-        attr_accessible :scripture_references_attributes
         has_many :scripture_references, :as => :scripturable, :dependent => :destroy
         accepts_nested_attributes_for :scripture_references, :allow_destroy => true
       end
